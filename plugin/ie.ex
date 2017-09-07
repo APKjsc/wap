c8669f1a50e03faf163b88544314a64ae14ab9a6
3T <lu@ng.huu.vn>
2017-09-07 12:12:19 +0700

    þ

---
+++ plugin/ie.ex
@@ -0,0 +1,12 @@
+<div class="smallNav">
+	<ui>
+		<li><a href="mailto:{{Mailto}}" target="blank"><i class="i i-envelope i-1x"><h2>Email</h2></i></a>
+		</li>
+		<li><a href="https://www.facebook.com/search/pages/?q={{Socials}}" target="blank"><i class="i i-facebook i-1x"> <h2>Facebook</h2></i></a>
+		</li>
+		<!--li><a href="https://instagram.com/{{Socials}}" target="blank"><i class="i i-instagram i-1x"><h2>Instagram</h2></i></a>
+		</li-->
+		<li><a instagram="instagram" href="!" ng-click="Instagram()"><i class="i i-About i-1x"><h2>About</h2></i></a>
+		</li>
+	</ui>
+</div>
---
+++ plugin/shell.ex
@@ -0,0 +1,15 @@
+<div ng-controller="about">
+	<div ng-class="{'scroll': page=='about'}" class="aboutOuter">
+		<div class="about">
+			<div abouts="abouts" ng-class="{'hide': page!='about'}" class="aboutInner">
+				<p>WE is a team based in HCMC.</p>
+				<p>We are a diverse team of artists, designers, and developers dedicated to facilitating a deeper relationship between culture and technology through critical analysis and innovation.</p>
+				<p class="twitter"></p>
+				<p>WE is available for creative and technical consulting to agencies, brands and individuals.</p>
+				<div class="social desktop"><a href="mailto:{{Mailto}}" target="blank"><i class="i i-envelope"><h2>email </h2></i></a><a href="https://www.facebook.com/search/pages/?q={{Socials}}" target="blank"><i class="i i-facebook"><h2>facebook </h2></i></a><a href="https://instagram.com/{{Socials}}" target="blank"><i class="i i-instagram"><h2>instagram </h2></i></a><a instagram="instagram" href="!" ng-click="Instagram()"><i class="i i-About"><h2>instagram </h2></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div ng-class="{'visible': page=='about'}" class="glowBar"></div>
+</div>
