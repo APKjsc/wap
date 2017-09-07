7bcff68d1bd5c8a06447a29506518937964ecc90
3T <lu@ng.huu.vn>
2017-09-07 12:34:15 +0700

    þ

--- plugin/ie.ex
+++ plugin/ie.ex
@@ -2,11 +2,11 @@
 	<ui>
 		<li><a href="mailto:{{Mailto}}" target="blank"><i class="i i-envelope i-1x"><h2>Email</h2></i></a>
 		</li>
-		<li><a href="https://www.facebook.com/search/pages/?q={{Socials}}" target="blank"><i class="i i-facebook i-1x"> <h2>Facebook</h2></i></a>
+		<li><a href="https://www.facebook.com/{{Socials}}" target="blank"><i class="i i-facebook i-1x"> <h2>Facebook</h2></i></a>
 		</li>
 		<!--li><a href="https://twitter.com/{{Socials}}" target="blank"><i class="i i-twitter i-1x"><h2>Twitter</h2></i></a>
 		</li-->
-		<li><a instagram="instagram" href="!" ng-click="Instagram()"><i class="i i-About i-1x"><h2>About</h2></i></a>
+		<li><a instagram="instagram" href="!" ng-click="Instagram()"><i class="i i-Instagram i-1x"><h2>Instagram</h2></i></a>
 		</li>
 	</ui>
 </div>
--- plugin/shell.ex
+++ plugin/shell.ex
@@ -6,7 +6,7 @@
 				<p>We are a diverse team of artists, designers, and developers dedicated to facilitating a deeper relationship between culture and technology through critical analysis and innovation.</p>
 				<p class="twitter"></p>
 				<p>WE is available for creative and technical consulting to agencies, brands and individuals.</p>
-				<div class="social desktop"><a href="mailto:{{Mailto}}" target="blank"><i class="i i-envelope"><h2>email </h2></i></a><a href="https://www.facebook.com/search/pages/?q={{Socials}}" target="blank"><i class="i i-facebook"><h2>facebook </h2></i></a><a href="https://twitter.com/{{Socials}}" target="blank"><i class="i i-twitter"><h2>twitter </h2></i></a><a instagram="instagram" href="!" ng-click="Instagram()"><i class="i i-About"><h2>instagram </h2></i></a>
+				<div class="social desktop"><a href="mailto:{{Mailto}}" target="blank"><i class="i i-envelope"><h2>email </h2></i></a><a href="https://www.facebook.com/{{Socials}}" target="blank"><i class="i i-facebook"><h2>facebook </h2></i></a><a href="https://twitter.com/{{Socials}}" target="blank"><i class="i i-twitter"><h2>twitter </h2></i></a><a instagram="instagram" href="!" ng-click="Instagram()"><i class="i i-Instagram"><h2>instagram </h2></i></a>
 				</div>
 			</div>
 		</div>
